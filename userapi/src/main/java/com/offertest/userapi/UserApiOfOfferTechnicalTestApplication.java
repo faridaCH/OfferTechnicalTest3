3a6783c9f3e941c900e275855c9e851cf71bf612
faridaCH <farida.chelhi06@gmail.com>
2022-11-23 16:11:41 +0100

    add a executable  project (spring boot project)

---
+++ userapi/src/main/java/com/offertest/userapi/UserApiOfOfferTechnicalTestApplication.java
@@ -0,0 +1,13 @@
+package com.offertest.userapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApiOfOfferTechnicalTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApiOfOfferTechnicalTestApplication.class, args);
+	}
+
+}
---
+++ userapi/src/test/java/com/offertest/userapi/UserApiOfOfferTechnicalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.offertest.userapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiOfOfferTechnicalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
